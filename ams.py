259d44a1cf6391b1ebb048add2f32defffd0dcf4
Yang Cao <gunale0926@hotmail.com>
2025-05-05 19:32:35 -0400

    Update ams.py

--- ams.py
+++ ams.py
@@ -129,21 +129,21 @@ class AMS(Optimizer):
                 if beta3 == beta1 or scaling == 1:
                     # Adam
                     p.addcdiv_(exp_avg, denom, value=-step_size)
-                elif beta3 > 0 and beta3 != beta1:
-                    # General version AMS
-                    phi = (exp_avg.sign() * exp_sign.sign() > 0).to(grad.dtype)
+                elif beta3 == 0 and scaling == -1:
+                    # Grams
+                    grad.sign_().mul_(exp_avg.abs())
+                    p.addcdiv_(grad, denom, value=-step_size)
+                elif beta3 == 0 and scaling != -1:
+                    # Gradient AMS (GAMS)
+                    phi = (exp_avg.sign() * grad.sign() > 0).to(grad.dtype)
                     psi = ((1 - phi) * scaling).to(grad.dtype)
                     total_mask = (phi + psi) * (
                         phi.numel() / (phi.sum() + psi.sum().abs() + 1)
                     )
                     p.addcdiv_(exp_avg * total_mask, denom, value=-step_size)
-                elif scaling == -1:
-                    # Grams
-                    grad.sign_().mul_(exp_avg.abs())
-                    p.addcdiv_(grad, denom, value=-step_size)
                 else:
-                    # General version AMS w.o. exp_sign
-                    phi = (exp_avg.sign() * grad.sign() > 0).to(grad.dtype)
+                    # AMS
+                    phi = (exp_avg.sign() * exp_sign.sign() > 0).to(grad.dtype)
                     psi = ((1 - phi) * scaling).to(grad.dtype)
                     total_mask = (phi + psi) * (
                         phi.numel() / (phi.sum() + psi.sum().abs() + 1)
